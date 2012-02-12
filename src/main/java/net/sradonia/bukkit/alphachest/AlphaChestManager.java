83f1dd6b73da9dc67f00d4af7b0cd87607ff36d6
kroimon <git@kroimon.de>
2012-02-12 12:52:45 +0100

    Switched from plaintext files to NBT storage. This allows us to store enchantments and possible future enhancements with chest items.

--- src/main/java/net/sradonia/bukkit/alphachest/AlphaChestManager.java
+++ src/main/java/net/sradonia/bukkit/alphachest/AlphaChestManager.java
@@ -1,16 +1,17 @@
 package net.sradonia.bukkit.alphachest;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.FilenameFilter;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
+import java.util.Map.Entry;
 import java.util.logging.Logger;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
 import net.minecraft.server.InventoryLargeChest;
 import net.minecraft.server.ItemStack;
+import net.minecraft.server.NBTBase;
+import net.minecraft.server.NBTTagCompound;
+import net.minecraft.server.NBTTagList;
 import net.minecraft.server.TileEntityChest;
 
 public class AlphaChestManager {
@@ -46,50 +47,76 @@ public class AlphaChestManager {
 	public void load() {
 		chests.clear();
 
-		int loadedChests = 0;
-
 		dataFolder.mkdirs();
-		final FilenameFilter filter = new FilenameFilter() {
-			public boolean accept(File dir, String name) {
-				return name.endsWith(".chest");
-			}
-		};
-		for (File chestFile : dataFolder.listFiles(filter)) {
+		for (File chestFile : dataFolder.listFiles()) {
 			try {
-				final InventoryLargeChest chest = new InventoryLargeChest("Virtual Chest", new TileEntityChest(), new TileEntityChest());
-				final String playerName = chestFile.getName().substring(0, chestFile.getName().length() - 6);
-
-				final BufferedReader in = new BufferedReader(new FileReader(chestFile));
-
-				String line;
-				int field = 0;
-				while ((line = in.readLine()) != null) {
-					if (line != "") {
-						final String[] parts = line.split(":");
-						try {
-							int type = Integer.parseInt(parts[0]);
-							int amount = Integer.parseInt(parts[1]);
-							short damage = Short.parseShort(parts[2]);
-							if (type != 0) {
-								chest.setItem(field, new ItemStack(type, amount, damage));
-							}
-						} catch (NumberFormatException e) {
-							// ignore
-						}
-						++field;
-					}
+				String chestFileName = chestFile.getName();
+				if (chestFileName.endsWith(".chest.nbt")) {
+					// Old plaintext file format
+					String playerName = chestFileName.substring(0, chestFile.getName().length() - 10);
+					chests.put(playerName.toLowerCase(), loadChestFromNBT(chestFile));
+				} else if (chestFileName.endsWith(".chest")) {
+					// New NBT file format
+					String playerName = chestFileName.substring(0, chestFile.getName().length() - 6);
+					chests.put(playerName.toLowerCase(), loadChestFromTextfile(chestFile));
 				}
-
-				in.close();
-				chests.put(playerName.toLowerCase(), chest);
-
-				++loadedChests;
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 
-		log.info("[Alpha Chest] loaded " + loadedChests + " chests");
+		log.info("[AlphaChest] loaded " + chests.size() + " chests");
+	}
+
+	private InventoryLargeChest loadChestFromTextfile(File chestFile) throws IOException {
+		final InventoryLargeChest chest = new InventoryLargeChest("Virtual Chest", new TileEntityChest(), new TileEntityChest());
+
+		final BufferedReader in = new BufferedReader(new FileReader(chestFile));
+
+		String line;
+		int slot = 0;
+		while ((line = in.readLine()) != null) {
+			if (!line.equals("")) {
+				final String[] parts = line.split(":");
+				try {
+					int type = Integer.parseInt(parts[0]);
+					int amount = Integer.parseInt(parts[1]);
+					short damage = Short.parseShort(parts[2]);
+					if (type != 0) {
+						chest.setItem(slot, new ItemStack(type, amount, damage));
+					}
+				} catch (NumberFormatException e) {
+					// ignore
+				}
+				++slot;
+			}
+		}
+
+		in.close();
+
+		return chest;
+	}
+
+	private InventoryLargeChest loadChestFromNBT(File chestFile) throws IOException {
+		final InventoryLargeChest chest = new InventoryLargeChest("Virtual Chest", new TileEntityChest(), new TileEntityChest());
+
+		final DataInputStream in = new DataInputStream(new GZIPInputStream(new FileInputStream(chestFile)));
+		final NBTTagCompound nbt = (NBTTagCompound) NBTBase.b(in);
+		in.close();
+
+		final NBTTagList items = nbt.getList("Items");
+
+		int chestSize = chest.getSize();
+		for (int i = 0; i < items.size(); i++) {
+			NBTTagCompound item = (NBTTagCompound) items.get(i);
+			byte slot = item.getByte("Slot");
+			if (slot > 0 && slot < chestSize) {
+				ItemStack itemStack = ItemStack.a(item);
+				chest.setItem(slot, itemStack);
+			}
+		}
+
+		return chest;
 	}
 
 	public void save() {
@@ -97,25 +124,18 @@ public class AlphaChestManager {
 
 		dataFolder.mkdirs();
 
-		for (String playerName : chests.keySet()) {
-			final InventoryLargeChest chest = chests.get(playerName);
+		for (Entry<String, InventoryLargeChest> entry : chests.entrySet()) {
+			final String playerName = entry.getKey();
+			final InventoryLargeChest chest = entry.getValue();
 
 			try {
-				final File chestFile = new File(dataFolder, playerName + ".chest");
-				if (chestFile.exists())
-					chestFile.delete();
-				chestFile.createNewFile();
+				// Delete the old plaintext file if it exists
+				final File textFile = new File(dataFolder, playerName + ".chest");
+				textFile.delete();
 
-				final BufferedWriter out = new BufferedWriter(new FileWriter(chestFile));
-
-				for (ItemStack stack : chest.getContents()) {
-					if (stack != null)
-						out.write(stack.id + ":" + stack.count + ":" + stack.getData() + "\r\n");
-					else
-						out.write("0:0:0\r\n");
-				}
-
-				out.close();
+				// Write the new chest file in NBT format
+				final File nbtFile = new File(dataFolder, playerName + ".chest.nbt");
+				saveChestToNBT(chest, nbtFile);
 
 				savedChests++;
 			} catch (IOException e) {
@@ -123,6 +143,30 @@ public class AlphaChestManager {
 			}
 		}
 
-		log.info("[Alpha Chest] saved " + savedChests + " chests");
+		log.info("[AlphaChest] saved " + savedChests + " chests");
 	}
+
+	private void saveChestToNBT(InventoryLargeChest chest, File chestFile) throws IOException {
+		final DataOutputStream out = new DataOutputStream(new GZIPOutputStream(new FileOutputStream(chestFile)));
+
+		NBTTagList items = new NBTTagList();
+
+		int chestSize = chest.getSize();
+		for (int slot = 0; slot < chestSize; slot++) {
+			ItemStack stack = chest.getItem(slot);
+			if (stack != null) {
+				NBTTagCompound item = new NBTTagCompound();
+				item.setByte("Slot", (byte) slot);
+				stack.b(item);
+				items.add(item);
+			}
+		}
+
+		final NBTTagCompound nbt = new NBTTagCompound();
+		nbt.set("Items", items);
+
+		NBTBase.a(nbt, out);
+		out.close();
+	}
+
 }

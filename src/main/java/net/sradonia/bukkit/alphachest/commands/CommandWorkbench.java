ec1727f10ce2ad8aae6aab7a1110afb7084c56e5
ShadowRanger <shadowranger@users.noreply.github.com>
2015-03-06 14:13:49 +1100

    Updated various aspects of the plugin
    - Made the plugin build with Java 7
    - Updated Bukkit API version to 1.8-R0.1-SNAPSHOT
    - Updated Maven Compiler Plugin version
    - Made plugin depend on Bukkit instead of CraftBukkit as the plugin
    doesn't use any of CraftBukkit's features
    - Renamed loadFromTextfile() to loadFromTextFile() in InventoryIO
    - Deprecated loadFromTextFile() in InventoryIO
    - Uppercased Teller Enums
    - Separated Commands into their own classes (for organisation purposes)
    - Refactored Commands
    - Added missing permission to plugin.yml: alphachest.chest.creativeMode
    - Made Teller Message colors configurable (new config options added)
    - Cleaned up pom.xml
    - Cleaned up plugin.yml
    - Updated README.md
    - General code refactoring

---
+++ src/main/java/net/sradonia/bukkit/alphachest/commands/CommandWorkbench.java
@@ -0,0 +1,34 @@
+package net.sradonia.bukkit.alphachest.commands;
+
+import net.sradonia.bukkit.alphachest.Teller;
+import net.sradonia.bukkit.alphachest.Teller.Type;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CommandWorkbench implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (command.getName().equalsIgnoreCase("workbench")) {
+			// Make sure the sender is a player
+			if (!(sender instanceof Player)) {
+				Teller.tell(sender, Type.ERROR, "Only players are able to open virtual workbenches.");
+				return true;
+			}
+				
+			Player player = (Player) sender;
+			
+			if (player.hasPermission("alphachest.workbench")) {
+				player.openWorkbench(null, true);
+			} else {
+				Teller.tell(sender, Type.ERROR, "You are not allowed to use this command.");
+			}
+			
+			return true;
+		}
+		return false;
+	}
+}

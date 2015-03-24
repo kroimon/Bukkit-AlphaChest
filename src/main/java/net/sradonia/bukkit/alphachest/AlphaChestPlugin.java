0331ec95dbd062125a918860fda0d67e4d41f9f4
ShadowRanger <shadowranger@users.noreply.github.com>
2015-03-24 14:06:36 +1100

    IntelliJ Code Reformat Test

--- src/main/java/net/sradonia/bukkit/alphachest/AlphaChestPlugin.java
+++ src/main/java/net/sradonia/bukkit/alphachest/AlphaChestPlugin.java
@@ -12,54 +12,54 @@ import net.sradonia.bukkit.alphachest.listeners.PlayerListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AlphaChestPlugin extends JavaPlugin {
-	
-	private Logger logger;
 
-	private VirtualChestManager chestManager;
+    private Logger logger;
 
-	private Teller teller;
+    private VirtualChestManager chestManager;
 
-	@Override
-	public void onEnable() {
-		// Save a copy of the default config.yml if one doesn't already exist
-		saveDefaultConfig();
+    private Teller teller;
+
+    @Override
+    public void onEnable() {
+        // Save a copy of the default config.yml if one doesn't already exist
+        saveDefaultConfig();
 
         // Initialize some classes and objects
         logger = getLogger();
 
-		File chestFolder = new File(getDataFolder(), "chests");
-		chestManager = new VirtualChestManager(chestFolder, logger);
-		
-		teller = new Teller(this);
+        File chestFolder = new File(getDataFolder(), "chests");
+        chestManager = new VirtualChestManager(chestFolder, logger);
 
-		// Set the plugin's command executors
-		getCommand("chest").setExecutor(new ChestCommand(chestManager));
-		getCommand("clearchest").setExecutor(new ClearChestCommand(chestManager));
-		getCommand("savechests").setExecutor(new SaveChestsCommand(chestManager));
-		getCommand("workbench").setExecutor(new WorkbenchCommand());
+        teller = new Teller(this);
 
-		// Register the plugin's events
-		getServer().getPluginManager().registerEvents(new PlayerListener(this, chestManager), this);
+        // Set the plugin's command executors
+        getCommand("chest").setExecutor(new ChestCommand(chestManager));
+        getCommand("clearchest").setExecutor(new ClearChestCommand(chestManager));
+        getCommand("savechests").setExecutor(new SaveChestsCommand(chestManager));
+        getCommand("workbench").setExecutor(new WorkbenchCommand());
 
-		// Schedule an auto-save task
-		int autosaveInterval = getConfig().getInt("autosave") * 1200;
+        // Register the plugin's events
+        getServer().getPluginManager().registerEvents(new PlayerListener(this, chestManager), this);
 
-		if (autosaveInterval > 0) {
-			getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
-				public void run() {
-					int savedChests = chestManager.save();
+        // Schedule an auto-save task
+        int autosaveInterval = getConfig().getInt("autosave") * 1200;
 
-					if (savedChests > 0 && !getConfig().getBoolean("silentAutosave")) {
+        if (autosaveInterval > 0) {
+            getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
+                public void run() {
+                    int savedChests = chestManager.save();
+
+                    if (savedChests > 0 && !getConfig().getBoolean("silentAutosave")) {
                         logger.info("Auto-saved " + savedChests + " chests");
                     }
-				}
-			}, autosaveInterval, autosaveInterval);
-		}
-	}
+                }
+            }, autosaveInterval, autosaveInterval);
+        }
+    }
 
-	@Override
-	public void onDisable() {
-		int savedChests = chestManager.save();
-		logger.info("Saved " + savedChests + " chests");
-	}
+    @Override
+    public void onDisable() {
+        int savedChests = chestManager.save();
+        logger.info("Saved " + savedChests + " chests");
+    }
 }
